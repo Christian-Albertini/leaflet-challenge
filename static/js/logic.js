1c258e51b5bc3cfb7bd6158aa48ff606b2a952cd
Christian Albertini <cja12classic@hotmail.com>
2024-07-22 21:46:07 -0400

    Commenting code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,23 +1,24 @@
 // API endpoint
 let queryUrl= "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-
+//Get request from API then run createFeatures with data
 d3.json(queryUrl).then(function(data){
     createFeatures(data.features);
 });
 
 function createFeatures(earthquakeData){
+    //Function that runs on each feature in array and creates popup with place, time, magnitude and depth
     function onEachFeature(feature, layer){
         layer.bindPopup(`<h3>Where: ${feature.properties.place}</h3><hr><p>Time: ${new Date(feature.properties.time)}</p><hr><p>Magnitude: ${feature.properties.mag}<p><hr><p>Depth: ${feature.geometry.coordinates[2]}<p>`);
     }
-
+    //Create GeoJSON layer and run on each feature and create the markers
     let earthquakes = L.geoJSON(earthquakeData, {
         onEachFeature: onEachFeature,
         pointToLayer: createMarkers
     });
-
+    //Run createMap using earthquakes layer
     createMap(earthquakes);
 }
-
+// Loop through features and set color for each marker based on depth
 function earthquakeColor(depth){
     if(-10<=depth && depth<=10){
         return '#7CFC00';
@@ -34,8 +35,9 @@ function earthquakeColor(depth){
     }
 }
 
-
+//Create markers
 function createMarkers(feature, coordinates){
+    //Set modifiers for each marker
     let modifiers ={
         radius: feature.properties.mag*8,
         fillColor: earthquakeColor(feature.geometry.coordinates[2]),
@@ -45,11 +47,11 @@ function createMarkers(feature, coordinates){
     }
     return L.circleMarker(coordinates, modifiers);
 }
-
+//Create Legend
 let legend = L.control({
     position: 'bottomright'
 });
-
+//Insert a div with the class of info legend to our legend
 legend.onAdd = function(map) {
 
     let div = L.DomUtil.create('div', 'info legend');
@@ -66,19 +68,20 @@ legend.onAdd = function(map) {
 return div;
 
 };
-
+//Function for creating Map
 function createMap(earthquakes){
+    //Define street layer
     let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
-    
+    //Define baseMaps object for base layer
     let baseMaps={
         "Street": street
     };
-
+    //Define overlayMaps object for overlay layer
     let overlayMaps = {
         "Earthquakes": earthquakes
     };
-
+    //Create map with base and earthquake layers
     let myMap= L.map("map", {
         center: [
             39.8282, -98.5795
@@ -86,9 +89,9 @@ function createMap(earthquakes){
         zoom: 4,
         layers: [street, earthquakes]
     });
-
+    //Create layer control that has base and overlay layers and add to map
     L.control.layers(baseMaps, overlayMaps).addTo(myMap);
-
+    //Add legend to map
     legend.addTo(myMap);
     
 }
